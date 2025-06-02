3c1f5b6ad0edfa71d1a51081c7f23caafc2f9bda
siiiirru <eqeee@naver.com>
2025-06-02 13:17:35 +0900

    [feat]rds테스트

--- src/main/java/actions/controller/ActionsController.java
+++ src/main/java/actions/controller/ActionsController.java
@@ -2,6 +2,8 @@ package actions.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @RestController
 public class ActionsController {
