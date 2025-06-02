f74703f81529eff45b70b4458303eb2987be3987
siiiirru <eqeee@naver.com>
2025-06-02 13:14:57 +0900

    [feat]rds테스트

--- src/main/java/actions/controller/ActionsController.java
+++ src/main/java/actions/controller/ActionsController.java
@@ -5,10 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ActionsController {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	
 	@GetMapping(value = "/api/actions")
 	public String actionsTest() {
 		return "app-with-actions version:0.0.1";
 	}
-	
+
+
+	@GetMapping("/db-test")
+	public String dbTest() {
+		String result = jdbcTemplate.queryForObject("SELECT NOW()", String.class);
+		return "DB 연결 성공! 현재 시간: " + result;
+	}
 }
